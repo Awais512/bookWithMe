991203af7ec48ce10aa2678fc886d49dcb5bab63
Awais512 <raza.awais25@gmail.com>
2018-12-23 17:29:03 +0500

    Rental-detail component added, Rental-list component added

---
+++ src/app/rental/rental-detail/rental-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { RentalService } from "../shared/rental.service";
+import { Rental } from "../shared/rental.model";
+
+@Component({
+  selector: "bwm-rental-detail",
+  templateUrl: "./rental-detail.component.html",
+  styleUrls: ["./rental-detail.component.scss"]
+})
+export class RentalDetailComponent implements OnInit {
+  rental: Rental;
+
+  constructor(
+    private route: ActivatedRoute,
+    private rentalService: RentalService
+  ) {}
+
+  ngOnInit() {
+    this.rental = new Rental();
+    this.route.params.subscribe(params => {
+      this.getRental(params["rentalId"]);
+    });
+  }
+
+  getRental(rentalId: any) {
+    this.rentalService.getRentalById(rentalId).subscribe((rental: Rental) => {
+      this.rental = rental;
+    });
+  }
+}
---
+++ src/app/rental/shared/rental.model.ts
@@ -0,0 +1,13 @@
+export class Rental {
+  id: any;
+  title: string;
+  city: string;
+  street: string;
+  category: string;
+  image: string;
+  bedrooms: number;
+  description: string;
+  dailyRate: number;
+  shared: boolean;
+  createdAt: string;
+}
